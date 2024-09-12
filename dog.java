1b21409792f0d7050be83acf68c14d37706722bc
BabyJoyDayao <babyjoydayao5@gmail.com>
2024-09-12 15:26:40 +0800

    first commit

---
+++ dog.java
@@ -0,0 +1,15 @@
+class Activity{
+    String name;
+    boolean isMale;
+    int num;
+    String name[] = new String[3];
+    Dog puppies;
+    
+
+    public String bark (String name, int num[]){
+        return "nice" + yourName + "my name is" + name;
+}
+    public String[showFriends()]{
+        return friends;
+    }
+}
