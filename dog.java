2101f4461e11d2c6d7979f8bb2a668fe95a0e9c9
BabyJoyDayao <babyjoydayao5@gmail.com>
2024-09-20 10:09:28 +0800

    Update dog.java

--- dog.java
+++ dog.java
@@ -1,15 +1,32 @@
-class Activity{
-    String name;
+class dog{
+    String Name;
     boolean isMale;
-    int num;
-    String name[] = new String[3];
-    Dog puppies;
-    
+    int numbersOfPuppies;
+    String [] listOfPuppies;
 
-    public String bark (String name, int num[]){
-        return "nice" + yourName + "my name is" + name;
-}
-    public String[showFriends()]{
-        return friends;
+    public void setName (String name){
+        Name = name;
     }
+
+    public String showName(){
+        return Name;
+    }  
+
+    public void bark(){
+        System.out.println( "woof! woof!");    
+    } 
+
+    public void setNumberOfPuppies(int numOfPuppies){
+        numbersOfPuppies = numOfPuppies;
+    }
+
+   public void setPuppies(String[] puppy){
+    listOfPuppies = puppy;
+   }
+
+   public void showPuppies(){
+    for(int i = 0; i < listOfPuppies.length; i++){
+        System.out.println("Puppie's Name: " + listOfPuppies.length);
+    }
+   }
 }
